72c8b25bd078566ba6c7e779468634e11ee436bc
jteo0 <jjtheodora@gmail.com>
2025-03-06 21:22:23 +0700

    minor fix to button

--- scenes/Level2.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready():
-	CurrentLevel.current_level = "res://scenes/Level2.tscn"
--- scenes/Level2Button.gd
+++ scenes/Level2Button.gd
@@ -1,4 +1,6 @@
 extends Button
 
 func _on_pressed() -> void:
+	if get_parent().name == "WinScreen":
+		CurrentLevel.current_level = "res://scenes/Level2.tscn"
 	get_tree().change_scene_to_file(CurrentLevel.current_level)
